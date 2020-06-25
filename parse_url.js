c9ff21ca3deaa04a0104790bb090dab085c0b866
andhika16 <moh.andhika16@gmail.com>
2020-06-25 19:38:10 +0700

    menambahkan parse_url untuk belajar parsing url

---
+++ parse_url.js
@@ -0,0 +1,19 @@
+const url = require('url');
+const adr = 'https://www.petanikode.com/search.php?year=2018&month=february';
+const q = url.parse(adr, true);
+
+
+console.log(`protokol : ${q.protocol}`);
+console.log(`hostname : ${q.host}`);
+console.log(`path : ${q.path}`);
+console.log(`params : ${q.search}`);
+
+const qData = q.query;
+console.log(qData);
+
+
+
+
+
+
+
