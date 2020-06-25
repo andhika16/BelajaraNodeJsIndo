527e0df30ed115f85cc7e2687ea135136542a8aa
andhika16 <moh.andhika16@gmail.com>
2020-06-25 19:26:46 +0700

    membuat server dan menambahkan file index.html

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+
+http.createServer((req, res) => {
+
+    fs.readFile('./index.html', (err, data) => {
+        if (err) return console.log(err);
+        res.writeHead(200, { 'Context-type': 'Text/html' })
+        res.write(data);
+        res.end();
+    })
+
+}).listen(3000);
+
+console.log('Server listening...');
+
+
+
+
+
+
+
